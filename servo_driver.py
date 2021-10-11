Corrected sign error on DH, testing small x-y motions

servo_driver.py
@@ -16,6 +16,7 @@ if (ON_RASPERRY_PI):
 from kinematics import Solver
 import numpy as np
 import math
+import sys
 
 class Driver():
     def __init__(self):
@@ -28,9 +29,9 @@ class Driver():
             print("WARNING: Running in Debug Configuration. WILL NOT WORK ON RASPBERRY PI.")
 
         # The global starting points for each letter we draw
-        # Offsets from our (0,0)
-        X_OFFSET = 0.35
-        Y_OFFSET = 0.
+        # Offsets from our (0,0) when theta0=theta2=0
+        X_OFFSET = 0.2140
+        Y_OFFSET = 0.0350
         self.base_points = [
             (X_OFFSET, Y_OFFSET + 0), 
             (X_OFFSET, Y_OFFSET + 1), 
@@ -38,6 +39,10 @@ class Driver():
             ]
 
         self.solver = Solver()
+
+        self.prev_theta0 = None
+        self.prev_theta1 = None
+        self.prev_theta2 = None
 
     def letter_to_points(self, letter):
         # pass
@@ -58,7 +63,7 @@ class Driver():
         MAX = 180.
         MIN = 0.
         theta_deg = max(MIN, min(MAX, np.rad2deg(theta_rad)))
-        print("Converted %.1f radians to %.1f degrees" %(theta_rad,theta_deg))
+        print("         Converted %.1f radians to %.1f degrees" %(theta_rad,theta_deg))
 
         return theta_deg
 
@@ -71,7 +76,7 @@ class Driver():
         while (theta1) > 2*math.pi:
             theta1 -= 2*math.pi
 
-        print("         Solve for thetas: %.1f, %.1f" % (theta0, theta1))
+        print("         Solved for thetas: %.1f, %.1f" % (theta0, theta1))
 
         theta0 = self.cap_and_convert_theta(np.array(theta0, dtype=float))
         theta1 = self.cap_and_convert_theta(np.array(theta1, dtype=float))
@@ -79,18 +84,24 @@ class Driver():
         return theta0, theta1
 
     def goto_point(self, p): # ignoring z since its up down
-        print("         going to point ", p)
+        print("Going to point ", p)
         # inverse kinematics to go from f(x,y) = [theta0, theta1]
         # we actually have a direct mapping from theta1 to x and theta2 to y
         theta0, theta1 = self.get_thetas(p)
 
         # TODO: There definitely need to be a mapping from the derived thetas and the what's sent to the servos
         if (ON_RASPERRY_PI):
-            self.axis0.angle = theta0
-            self.axis1.angle = theta1
+            if (self.prev_theta0 is not None and abs(self.prev_theta0 - theta0) > 0.1):
+                self.axis0.angle = theta0
+                
+            if (self.prev_theta1 is not None and abs(self.prev_theta1 - theta1) > 0.1):
+                self.axis1.angle = theta1
         else:
             angle = theta0
             angle = theta1
+
+        self.prev_theta0 = theta0
+        self.prev_theta1 = theta1
 
         
     def draw_letter(self, letter, reference_point):
@@ -129,15 +140,31 @@ class Driver():
                 th0, th2 = self.get_thetas([x,y])
                 print(f"{x:.2f}     {y:.2f}     {th0:.2f}      {th2:.2f}")
 
+    def get_zero_position(self):
+        # Based on forward kinematics what're the thetas associated with getting the eef to (0,0)
+        p = self.solver.zero_theta_position()
+        l3 = self.solver.l3_length_at(0.)
+        print(f"Zero theta puts eef at {p[0]:.4f}, {p[1]:.4f}. l3 starts out at length {l3:.4f}.")
+
 
 if __name__ == "__main__":
     d = Driver()
+
     word = "|-" # not implemented. Just draw a straight vertical line and a straight horizontal line
-    
     if ON_RASPERRY_PI:
         d.run(word)
     else:
-        d.produce_discrete_table()
+        d.get_zero_position()
+        # d.produce_discrete_table()
+        test_points = []
+        test_points.append((0.2140, 0.035)) # starting point for 0thetas
+        test_points.append((0.22, 0.035)) # left a little
+        test_points.append((0.20, 0.035)) # right a little 
+        test_points.append((0.2140, 0.07)) # down a little
+        test_points.append((0.2140, 0.1)) # up a little
+        for p in test_points:
+            d.goto_point(p)
+        # d.goto_point((float(sys.argv[1]), float(sys.argv[2])))
 
     
 